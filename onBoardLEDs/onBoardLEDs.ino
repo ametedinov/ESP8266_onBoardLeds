9b75124ab8aa708ca7100323098e1c4bca990281
ametedinov <ametedinov@gmail.com>
2016-07-04 03:59:32 -0400

    Updated Comments

--- onBoardLEDs/onBoardLEDs.ino
+++ onBoardLEDs/onBoardLEDs.ino
@@ -2,7 +2,9 @@
  * This program controls the on-board LEDs of the nodeMCU ESP8266 board.
  * The blue LED on the ESP12 chip is connected to GPIO2.
  * The red LEd on the NodeMCU breakout is on GPIO13 which is internally defined as LED_BUILTIN.
- * The LEDs have inverse logic. 0 corresponds to ON, 1 corresponds to OFF. * 
+ * The LEDs have inverse logic. 0 corresponds to ON, 1 corresponds to OFF. 
+ *    You can use Eclipse for Arduino to find the file where a particular function/constant
+ *    is defined. You simply Ctrl+click on the function and Eclipse opens the definition file.
  */
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
